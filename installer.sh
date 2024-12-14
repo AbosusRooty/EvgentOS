46ffaece40317877ece4559457c248195bbc7211
AbosusRooty <root39058@gmail.com>
2024-12-14 15:10:32 +0300

    not done

--- installer.sh
+++ installer.sh
@@ -1,11 +1,1 @@
-echo wait...
-wait 1
-echo installing git...
-wait 5
-pkg install git -y
-echo finding link...
-wait 1
-git clone https://github.com/khansaad1275/Termux-Games/
-cd Termux-Games
-chmod +x *
-bash install.sh
+echo 
