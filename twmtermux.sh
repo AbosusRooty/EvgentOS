f4fa18e74174835b8b46d8ea25e09e64f98a85f4
AbosusRooty <root39058@gmail.com>
2024-12-15 17:06:15 +0300

    Create twmtermux.sh

---
+++ twmtermux.sh
@@ -0,0 +1,16 @@
+echo updating...
+pkg update
+pkg upgrade
+echo installing aterm
+pkg install aterm
+echo aterm installed
+echo installing xorg-twm
+pkg install xorg-twm
+echo twm installed
+echo installing packages...
+pkg install xorg
+pkg install xorg-xclock
+pkg install firefox
+pkg install tigervnc
+pkg install neofetch
+pkg install wget
