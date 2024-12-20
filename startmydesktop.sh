329a7b3a794c061f27be6571ae9df56de97c2c85
AbosusRooty <root39058@gmail.com>
2024-12-20 21:53:51 +0300

    wallpaper
    Signed-off-by: AbosusRooty <root39058@gmail.com>

--- startmydesktop.sh
+++ startmydesktop.sh
@@ -1,4 +1,4 @@
 vncserver
 export DISPLAY=:1
-polybar & openbox & rofi & xcompmgr & xsetroot & dmenu & xfce4-panel
+polybar & openbox & rofi & xcompmgr & xsetroot & dmenu & xfce4-panel & xfdesktop
 echo follow @zhen6kreboot
