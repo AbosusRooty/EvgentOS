569bf5e7761fee9996f666be2e9ecc2f5f612cf4
AbosusRooty <root39058@gmail.com>
2024-12-20 21:52:34 +0300

    fixed
    Signed-off-by: AbosusRooty <root39058@gmail.com>

--- install.sh
+++ install.sh
@@ -1,47 +1,48 @@
 echo WELCOME TO ZHENYAOS
-pkg update && pkg upgrade
-pkg install x11-repo
-pkg install tigervnc
+pkg update && pkg upgrade -y
+pkg install x11-repo -y
+pkg install tigervnc -y
 clear
-echo installing openbox wm
-pkg install openbox obconf
-echo installing setter color back
-pkg install xorg-xsetroot
+echo installing openbox wm -y
+pkg install openbox obconf -y
+echo installing setter color back -y
+pkg install xorg-xsetroot -y
 echo install apps needed for zhenOS
-pkg install aterm
-pkg install xterm
-pkg install xcompmgr
-pkg install xfce4-settings
-pkg install polybar
-pkg install st
-pkg install geany
+pkg install aterm -y
+pkg install xterm -y
+pkg install xcompmgr -y
+pkg install xfce4-settings -y
+pkg install polybar -y
+pkg install st -y
+pkg install geany -y
 echo installing optional file manager
-pkg install thunar
+pkg install thunar -y
 echo done
-pkg install pacmanfm
-pkg install rofi
-pkg install feh
-pkg install neofetch
-pkg install git
-pkg install wget
-pkg install curl
-pkg install zsh
-pkg install vim
+pkg install pacmanfm -y
+pkg install rofi -y
+pkg install feh -y
+pkg install neofetch -y
+pkg install git -y
+pkg install wget -y
+pkg install curl -y
+pkg install zsh -y
+pkg install vim -y
 echo installing optional apps for best work
-pkg install htop
-pkg install elinks
-pkg install mutt
-pkg install mc
-pkg install ranger
-pkg install cmus
-pkg install cava
-pkg install pulseaudio
+pkg install htop -y
+pkg install elinks -y
+pkg install mutt -y
+pkg install mc -y
+pkg install ranger -y
+pkg install cmus -y
+pkg install cava -y
+pkg install pulseaudio -y
 echo installing firefox
-pkg install firefox
+pkg install firefox -y
 echo install apps for fun and best work
-pkg install dosbox
-pkg install the-powder-toy
-pkg install dmenu
+pkg install dosbox -y
+pkg install the-powder-toy -y
+pkg install dmenu -y
+pkg install xfdesktop -y
 pkg update -y && pkg upgrade -y
 pkg install git
 git clone https://github.com/FajarKim/games-termux
