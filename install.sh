01c4443d068a6be4a0f68d3d74d32998b736a3a5
AbosusRooty <root39058@gmail.com>
2025-01-22 17:10:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 echo WELCOME TO ZHENYAOS
-pkg update && pkg upgrade && pkg install x11-repo && pkg install tigervnc openbox obconf xorg-xsetroot xcompmgr xterm polybar st zsh geany pcmanfm rofi feh neofetch htop vim elinks mutt git wget curl xfce4-settings lxqt xfce4 shotcut jack jack2 jack-example-tools
+pkg update && pkg upgrade && pkg install x11-repo && pkg install tigervnc openbox obconf xorg-xsetroot xcompmgr xterm polybar st zsh geany pcmanfm rofi feh neofetch htop vim elinks mutt git wget curl xfce4-settings lxqt xfce4 -y
 echo done! type ./startmydesktop.sh to use
