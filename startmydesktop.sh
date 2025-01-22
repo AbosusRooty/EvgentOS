65b32e01706300a99d06bd42c7a872c4d6f346ff
AbosusRooty <root39058@gmail.com>
2025-01-22 16:14:15 +0300

    fixed pane
    Signed-off-by: AbosusRooty <root39058@gmail.com>

--- startmydesktop.sh
+++ startmydesktop.sh
@@ -1,4 +1,4 @@
 vncserver
 export DISPLAY=:1
-polybar & openbox & rofi & xcompmgr & xsetroot & dmenu & xfce4-panel & xfdesktop
+polybar & openbox & rofi & xcompmgr & xsetroot & dmenu & lxqt-panel & xfdesktop
 echo follow @zhen6kreboot
