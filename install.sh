6e98b9035b72c5aa4d736a011a4aaae7463d93a3
AbosusRooty <root39058@gmail.com>
2025-03-14 23:39:22 +0300

    Fixed, deleted background, added kali
    Signed-off-by: AbosusRooty <root39058@gmail.com>

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 echo WELCOME TO ZHENYAOS
-pkg update && pkg upgrade && pkg install x11-repo && pkg install tigervnc openbox obconf xorg-xsetroot xcompmgr xterm polybar st zsh geany pcmanfm rofi feh neofetch htop vim elinks mutt git wget curl xfce4-settings lxqt xfce4 -y
-echo done! type ./startmydesktop.sh to use
+pkg update && pkg upgrade && pkg install x11-repo && pkg install tigervnc openbox obconf xorg-xsetroot xcompmgr xterm polybar st zsh geany pcmanfm rofi feh neofetch htop vim elinks mutt git wget curl xfce4-settings xfce4-panel -y
+echo done! type "./startmydesktop.sh" to use
+echo installing nethunter(rootless NOT HACKING!)
+termux-setup-storage
+pkg install wget
+wget -O install-nethunter-termux https://offs.ec/2MceZWr
+chmod +x install-nethunter-termux
+./install-nethunter-termux
+echo installed EvgentOS + KaliNethunter
