3b29d78f701fd9f66424142fe08d95b6c700a78b
AbosusRooty <root39058@gmail.com>
2025-03-14 23:40:52 +0300

    Xfce4panel + nethunter
    Signed-off-by: AbosusRooty <root39058@gmail.com>

--- startmydesktop.sh
+++ startmydesktop.sh
@@ -1,4 +1,4 @@
 vncserver
 export DISPLAY=:1
-polybar & openbox & rofi & xcompmgr & xsetroot & dmenu & lxqt-panel & xfdesktop
-echo follow @zhen6kreboot
+polybar & openbox & rofi & xcompmgr & xsetroot & dmenu & xfce4-panel & nethunter kex &
+
